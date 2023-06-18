93cc8f16b06782075e5d41f47d228d3051e70dab
w444555888888 <w444555888@yahoo.com.tw>
2023-06-18 20:44:58 +0800

    [Update]do again
    [Update]do again

--- shopping.js
+++ shopping.js
@@ -29,14 +29,15 @@ let empty = []
 
 function page () {
   let total = ''
+
   goods.forEach(ele => {
     total += `<li class="meal" data-uname=${ele.id}>
-            <img src=${ele.img} alt="餐點 3" style="width: 400px;height: 300px;">
-            <h3 class="food">${ele.food}</h3>
-            <h3 class="price">價格:$${ele.price}</h3>
-            <input class="count" type="number" min="0" value="${ele.count}">
-            <button class="add-to-cart" disabled="${ele.count == 0}">加入購物車</button>
-          </li>`
+  <img src=${ele.img} alt="餐點 3" style="width: 400px;height: 300px;">
+  <h3 class="food">${ele.food}</h3>
+  <h3 class="price">價格:$${ele.price}</h3>
+  <input class="count" type="number" min="1" value="${ele.count}">
+  <button class="add-to-cart">加入購物車</button>
+</li>`
   })
 
   $('#meal-list').html(total)
@@ -45,10 +46,10 @@ function page () {
 page()
 
 
-$('.pay').click(function () {
-  $('.window').css('display', 'flex').css('flex-direction', 'row').css('justify-content', 'flex-start').css('align-items', 'flex-start')
-})
 
+$('.pay').click(function () {
+  $('.modal').addClass('plusClass')
+})
 
 
 $('#meal-list').on('click', '.add-to-cart', function () {
@@ -57,100 +58,65 @@ $('#meal-list').on('click', '.add-to-cart', function () {
   let price = $(this).parent('.meal').find('.price').text().slice(4)
   let count = $(this).parent('.meal').find('input').val()
 
-  pushShoppingCart(id, food, price, count)
-  renderCart()
-  finallyPrice()
-})
+  console.log(empty)
 
 
+  
+  // id 不重複累加
+  let existing = empty.find(ele => ele.id === id)
 
-$('#meal-list').on('change', '.count', function () {
-  let count = $(this).val()
-  let btn = $(this).parent('.meal').find('.add-to-cart')
-
-  if (count > 0) {
-    btn.prop('disabled', false)
+  if (existing) {
+    existing.count += Number(count)
   } else {
-    btn.prop('disabled', true)
+    empty.push({
+      id: id,
+      food: food,
+      price: price,
+      count: Number(count)
+    })
   }
+
+  display()
+
+  finallyPrice()
+
+
 })
 
 
 
-function renderCart () {
-  let total = ''
 
 
-  empty.forEach((item) => {
-    let totalPrice = item.price * item.count // 計算價格乘以數量的結果
 
-    total += `<li>
-                <h3>商品:${item.food}</h3>
-                <h3>價格:${item.price}</h3>
-                <h3>數量:${item.count}</h3>
-                <h3>總價:${totalPrice}</h3> 
-                </li>`
-
+function display () {
+  total = ''
+  let totalPrice = 0
+  empty.forEach(ele => {
+    totalPrice = Number(ele.count * ele.price)
+    total += `
+    <div>商品名:${ele.food}</div>
+    <div>單價:${ele.price}</div>
+    <div>數量:${ele.count}</div>
+    <div>總價:${totalPrice}</div>
+    `
   })
 
-
-  $('.window').html(total)
-
-
-
-
+  $('.modal').html(total)
 
 }
 
 
-// empty為空值push(id, food, price, count)進去
-function pushShoppingCart (id, food, price, count) {
-  if (empty.length == 0) {
-    empty.push({
-      id,
-      food,
-      price,
-      count
-    })
-  }
 
-  // findIndex沒有找到返回-1
-  let hasGood = empty.findIndex(e => e.id == id)
-  if (hasGood >= 0) {
-    console.log(hasGood)
-    empty[hasGood].count = Number(empty[hasGood].count) + Number(count)
-  } else {
-    empty.push({
-      id,
-      food,
-      price,
-      count
-    })
-  }
-}
-
-// 總價
+// 全部結帳金額
 function finallyPrice () {
 
-  let totalfinally = 0
+  let finallycount = 0
 
-
-  empty.forEach((item) => {
-    let total = item.count * item.price
-    totalfinally += Number(total)
+  empty.forEach(ele => {
+    let total = ele.count * ele.price
+    finallycount += Number(total)
   })
 
-
-  $('.total-price').text(`總價: ${totalfinally}`)
-
+  $('#total-price').html('總價:$' + finallycount)
 
 }
-
-
-
-
-
-
-
-
-
